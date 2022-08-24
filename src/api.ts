1e49df0891d492fdd04e60d42e4432d182e3996d
Thamyres Guedes <thataguedis@gmail.com>
2022-08-24 08:05:24 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://nextjs-baselayout-git-main-thamys.vercel.app/api",
+  baseURL: "https://nextjs-baselayout.vercel.app/api",
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
