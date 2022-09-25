577a66e54e8fb6887b9ba0c7b362e1363a5ef169
Thamyres Guedes <thataguedis@gmail.com>
2022-09-25 20:40:12 -0300

    Config primary color as the user is logged in

---
+++ src/store/ducks/auth.ts
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createReducer } from "@reduxjs/toolkit";
+import { API } from "../../api";
+import { handleApiError } from "../../utils/error";
+import { ApiError, DefaultState } from "../../types/store";
+import { AxiosResponse } from "axios";
+
+type InitialState = DefaultState & {
+  user: any;
+};
+
+export const INITIAL_STATE: InitialState = {
+  status: "idle",
+  error: null,
+  user: null,
+};
+
+export const postLoginAction = createAsyncThunk<
+  AxiosResponse<any>,
+  any,
+  { rejectValue: ApiError }
+>("auth/login", async (form, thunkAPI) => {
+  try {
+    return await API.post("/auth");
+  } catch (error) {
+    return thunkAPI.rejectWithValue(handleApiError(error));
+  }
+});
+
+// REDUCER
+export const authReducer = createReducer<InitialState>(
+  INITIAL_STATE,
+  (builder) => {
+    // Empresas
+    builder.addCase(postLoginAction.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.user = action.payload.data;
+      state.error = null;
+    });
+    builder.addCase(postLoginAction.pending, (state) => {
+      state.status = "loading";
+      state.error = null;
+    });
+    builder.addCase(postLoginAction.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = action.payload;
+    });
+  }
+);
--- src/store/ducks/reducers.ts
+++ src/store/ducks/reducers.ts
@@ -1,13 +1,15 @@
 // Reducers list
 import { exampleReducer } from "./example";
 import { menuReducer } from "./menu";
+import { authReducer } from "./auth";
 
 /**
  * REDUCERS LIST
  */
 const reducersList = {
   exampleReducer,
-  menuReducer
+  menuReducer,
+  authReducer
 };
 
 export default reducersList;
