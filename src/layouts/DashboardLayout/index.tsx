4c29153eaed93421e66a2ae0b50ed4513be403cd
Thamyres Guedes <thataguedis@gmail.com>
2022-09-25 20:39:43 -0300

    Config primary color as the user is logged in

--- src/layouts/DashboardLayout/index.tsx
+++ src/layouts/DashboardLayout/index.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { Layout } from "antd";
+import React, { useEffect } from "react";
+import { ConfigProvider, Layout } from "antd";
 import Sidebar from "../../containers/Sidebar";
 import Header from "../../containers/Header";
 import styles from "./styles.module.css";
 import Link from "next/link";
+import { RootState, useAppSelector } from "../../store/index";
 
 const { Footer, Content } = Layout;
 
 const DashboardLayout = ({ children }) => {
+  const { user } = useAppSelector((state: RootState) => state.authReducer);
+
+  useEffect(() => {
+    ConfigProvider.config({
+      theme: {
+        primaryColor: user.primaryColor,
+      },
+    });
+  }, [user]);
+
   return (
     <Layout className={styles.layout}>
       <Sidebar />
